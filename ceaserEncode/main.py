4c2ac2a642c11b8bc83a00b8234c56a944e24dff
Hennadii Borysevych <Hennadii.Borysevych@ventionteams.com>
2023-12-11 13:11:38 +0100

    refactor: combined encode and decode functions to one

--- ceaserEncode/main.py
+++ ceaserEncode/main.py
@@ -7,37 +7,45 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# Don't change the code above 👆
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_text = ''
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-    # TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.
-    # e.g.
-    # plain_text = "hello"
-    # shift = 5
-    # cipher_text = "mjqqt"
-    # print output: "The encoded text is mjqqt"
-    cipher_text = ""
-    for letter in plain_text:
+    if direction == 'decode':
+        shift_amount *= -1
+
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
 
+    print(f"The {cipher_direction} text is {end_text}")
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# def encrypt(plain_text, shift_amount):
+#     # TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.
+#     # e.g.
+#     # plain_text = "hello"
+#     # shift = 5
+#     # cipher_text = "mjqqt"
+#     # print output: "The encoded text is mjqqt"
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
+#
+#
+# def decrypt(cipher_text, shift_amount):
+#     plain_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print(f"The decoded text is {plain_text}")
 
 
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
+ceaser(start_text=text, shift_amount=shift, cipher_direction=direction)
